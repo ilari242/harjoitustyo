22a7877737621c2e7cb2748e50f55b3b96eeef2e
ilari242 <ilari242@hotmail.com>
2017-04-04 15:53:25 +0300

    Update databasehandler.php

--- core/database/databasehandler.php
+++ core/database/databasehandler.php
@@ -1,7 +1,7 @@
 <?php
 //$conn = pg_connect("dbstud2.sis.uta.fi","5432", "il99590", "il99590", "password=kukka");
 //$conn = mysqli_connect("localhost", "il99590", "Lyijykyna1", "il99590");
-$conn = pg_connect("dbstud2.sis.uta.fi", "5432", "kukka", "il99590");
+$conn = pg_connect("dbstud2.sis.uta.fi", "5432", "il99590", "kukka");
 
 if (!$conn) {
     die("Connection failed: ". pg_result_error_field());
