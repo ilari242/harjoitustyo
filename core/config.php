3484b7f11468838781cdacc5bd2f5dd84d1626eb
ilari242 <ilari242@hotmail.com>
2017-04-04 14:47:22 +0300

    Update config.php

--- core/config.php
+++ core/config.php
@@ -5,7 +5,6 @@ return [
 		'username' => 'il99590',
 		'password' => 'Lyijykyna1',
 		'connection' => 'pgsql:host=dbstud2.sis.uta.fi', //'mysql:host=127.0.0.1',
-		'port' => '5432',
 		'options' => [
 			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 		]
