5079caebb1cf7f616d3dec13c69d8b958008e97e
ilari242 <ilari242@hotmail.com>
2017-04-04 21:13:53 +0300

    Update signup.inc.php

--- app/includes/signup.inc.php
+++ app/includes/signup.inc.php
@@ -24,14 +24,14 @@ if (empty($password)){
     exit();
 }
 else {
-    $result = pg_query("SELECT uid FROM user WHERE uid='$uid'");
+    $result = pg_query($conn, "SELECT uid FROM user WHERE uid='$uid'");
     $uidcheck = pg_num_rows($result);
     if ($uidcheck > 0) {
         header("Location: ../../signup.php?error=username");
         exit();
     } else {
         $encrypted_password = password_hash($password, PASSWORD_DEFAULT);
-        $result = pg_query("INSERT INTO user (first, last, uid, password) 
+        $result = pg_query($conn, "INSERT INTO user (first, last, uid, password) 
         VALUES ('$first', '$last', '$uid', '$encrypted_password')");
         echo "$result";
         //header("Location: ../../index.php");
