261df0102062a9ca60c06fe46e1a1c70c84f5e87
ilari242 <ilari242@hotmail.com>
2017-04-04 22:39:04 +0300

    Update login.inc.php

--- app/includes/login.inc.php
+++ app/includes/login.inc.php
@@ -5,7 +5,7 @@ include '../../core/database/databasehandler.php';
 $uid = pg_escape_string($conn, $_POST['uid']);
 $password = pg_escape_string($conn, $_POST['password']);
 '" . $name . "';";
-$result = pg_query($conn, "SELECT * FROM \"user\" WHERE uid='" . $uid . "';");
+$result = pg_query($conn, "SELECT * FROM \"user\" WHERE uid='" . $uid . "'");
 $row = $result->fetch_assoc();
 $hash_password = $row['password'];
 $hash = password_verify($password, $hash_password);
@@ -14,7 +14,7 @@ if ($hash == 0){
     header("Location: ../../index.php?error=empty");
     exit();
 } else {
-    $result = pg_query($conn, "SELECT * FROM \"user\" WHERE uid='" . $uid . "' AND password='" . $uid . "';");
+    $result = pg_query($conn, "SELECT * FROM \"user\" WHERE uid='" . $uid . "' AND password='" . $uid . "'");
 
     if (!$row = pg_fetch_assoc($result)) {
         echo "Käyttäjänimi tai salasana on väärin!";
