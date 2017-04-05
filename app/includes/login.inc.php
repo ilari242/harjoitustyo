1af1709e6b5687cd8cc9752c35ad5b84f5acd9b2
ilari242 <ilari242@hotmail.com>
2017-04-05 20:08:38 +0300

    Update login.inc.php

--- app/includes/login.inc.php
+++ app/includes/login.inc.php
@@ -4,14 +4,10 @@ include '../../core/database/databasehandler.php';
 
 $uid = pg_escape_string($conn, $_POST['uid']);
 $password = pg_escape_string($conn, $_POST['password']);
-echo "$uid";
-echo "$password";
 $result = pg_query($conn, "SELECT * FROM usertable WHERE uid='$uid'");
 $row = pg_fetch_assoc($result);
-echo "$row";
 $hash_password = $row['password'];
 $hash = password_verify($password, $hash_password);
-echo "$hash";
 
 if ($hash == 0){
     header("Location: ../../index.php?error=empty");
