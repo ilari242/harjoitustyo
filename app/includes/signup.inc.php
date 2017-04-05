6d1a59ff1461c7b8c4ffbc1e75f103fa89afa558
ilari242 <ilari242@hotmail.com>
2017-04-05 20:16:04 +0300

    Update signup.inc.php

--- app/includes/signup.inc.php
+++ app/includes/signup.inc.php
@@ -24,7 +24,7 @@ if (empty($password)){
     exit();
 }
 else {
-    $sql = "SELECT uid FROM usertable WHERE uid='$uid'"
+    $sql = "SELECT uid FROM usertable WHERE uid='$uid'";
     $result = pg_query($conn, $sql);
     $uidcheck = pg_num_rows($result);
     if ($uidcheck > 0) {
