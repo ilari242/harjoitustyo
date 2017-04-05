a33c5cd934563164e670e2112ef02edd795ee886
ilari242 <ilari242@hotmail.com>
2017-04-05 17:08:22 +0300

    Update index.view.php

--- app/resources/views/index.view.php
+++ app/resources/views/index.view.php
@@ -24,7 +24,7 @@
                 </p>
                 <br>
                 <?php foreach ($users as $user):?>
-                    <?php if ($_SESSION['id'] != $user->id) {?>
+                    <?php if ($_SESSION['id'] != $'user'->id) {?>
                         <label class="panel-block">
                             <?= htmlspecialchars($user['first']); ?>
                             <?= htmlspecialchars($user['last']); ?>
