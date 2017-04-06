1e8767f154898d3d61af3aa1b59aa2a27f08f3b2
ilari242 <ilari242@hotmail.com>
2017-04-06 17:17:28 +0300

    Update Task.php

--- app/models/Task.php
+++ app/models/Task.php
@@ -5,13 +5,5 @@ class Task
 	public $completed;
 	public $id;
 
-	public function isCompleted()
-	{
-		return $this->completed;
-	}
-
-	public function complete()
-	{
-		$this->completed = true;
-	}
+	
 }
