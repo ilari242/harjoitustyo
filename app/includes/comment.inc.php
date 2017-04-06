8bfdd334f9b145a3068d2a5c3ed773f6aaadabc5
ilari242 <ilari242@hotmail.com>
2017-04-06 17:46:46 +0300

    Update comment.inc.php

--- app/includes/comment.inc.php
+++ app/includes/comment.inc.php
@@ -4,7 +4,7 @@ function setComments($conn) {
     if (isset($_POST['commentSubmit'])) {
         $uid = $_POST['uid'];
         $date = $_POST['date'];
-        $message = $_POST['message'];
+        $message = pg_escape_string($conn, $_POST['message']);
 
         $sql = "INSERT INTO comment (uid, date, message) VALUES ('$uid', '$date', '$message')";
         $result = pg_query($conn, $sql);
