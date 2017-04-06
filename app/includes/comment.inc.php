e99d7f3d73da91a78e362e166b525589d80c84ef
ilari242 <ilari242@hotmail.com>
2017-04-06 16:29:16 +0300

    Update comment.inc.php

--- app/includes/comment.inc.php
+++ app/includes/comment.inc.php
@@ -18,8 +18,9 @@ function getComments($conn) {
         $id = $row['uid'];
         $sql2 = "SELECT * FROM usertable WHERE id = '$id'";
         $result2 = pg_query($conn, $sql2);
-        if ($row2 = pg_fetch_assoc($result2)) 
+        if ($row2 = pg_fetch_assoc($result2)) {
         echo "<div class='comment-box'><p>";
+        }
         echo $row2['uid']."<br>";
         echo $row['date']."<br>";
         echo nl2br($row['message']);
